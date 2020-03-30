b9077537b5903c6af718d88083abf9b76395953d
ouwenkg <2630582710@qq.com>
2020-03-30 20:09:22 +0800

    use engine

--- main.go
+++ main.go
@@ -6,13 +6,26 @@ import (
 	"net/http"
 )
 
-func main() {
-	// $ curl http://localhost:9999/
-	http.HandleFunc("/", indexHandler)
+// Engine used deal all requests
+type Engine struct{}
 
-	// curl http://localhost:9999/hello
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
+// Handle http requests
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "url.path = %q\n", req.URL.Path)
+	case "hello":
+		for k, v := range req.Header {
+			fmt.Fprintln(w, "header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND = %s\n", req.URL.Path)
+	}
+}
+
+func main() {
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":9999", engine))
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
