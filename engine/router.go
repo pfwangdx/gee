b17006361814c7a89063360c9f48515c615a984e
ouwenkg <2630582710@qq.com>
2020-04-04 23:24:48 +0800

    Feat: udpate router sub and pub

--- engine/router.go
+++ engine/router.go
@@ -1,33 +1,63 @@
 package engine
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type router struct {
-	r map[string]HandlerFunc
+	roots map[string]*node
+	handlers map[string]HandlerFunc
 }
 
-
 func newRouter() *router {
-	return &router{ make(map[string]HandlerFunc)}
+	return &router{
+		roots: make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
 }
 
 // AddRoute about the handler func
-func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	r.r[key] = handler
+func (r *router) addRoute(method string, path string, handler HandlerFunc) {
+	// search_path := parsePath(path)
+	key := method + "-" + path
+
+	// roots operaions
+	
+	r.handlers[key] = handler
 }
 
-func (r *router) list() map[string]HandlerFunc {
-	return r.r
+func (r *router) getRoute(method string, path string) {
+
 }
+
+// func (r *router) list() map[string]HandlerFunc {
+// 	return r.r
+// }
 
 func (r *router) handle(context *Context) {
+	params := parsePath(context.req.URL.Path)
+
 	key := context.req.Method + "-" + context.req.URL.Path
 
-	if handler, ok := r.r[key]; ok {
+	if handler, ok := r.handlers[key]; ok {
 		handler(context)
 	} else {
 		fmt.Fprintf(context.response, "404 NOT FOUND = %s\n", context.req.URL.Path)
 	}
 }
 
+// parsePath to return search path of the real request path 
+func parsePath(path string) []string {
+	vs := strings.Split(path, "/")
+	search_path := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			search_path = append(search_path, item)
+			if item == "*" {
+				break
+			}
+		}
+	}
+	return search_path
+}
