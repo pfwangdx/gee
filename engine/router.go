c3dd3b3e01fa3a58627506cb8d47ce87cfbc4171
ouwenkg <2630582710@qq.com>
2020-04-06 23:07:01 +0800

    update router logic

--- engine/router.go
+++ engine/router.go
@@ -19,26 +19,51 @@ func newRouter() *router {
 
 // AddRoute about the handler func
 func (r *router) addRoute(method string, path string, handler HandlerFunc) {
-	// search_path := parsePath(path)
-	key := method + "-" + path
-
-	// roots operaions
+	searchPath := parsePath(path)
 	
+	key := method + "-" + path
+	// roots operaions
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node {}
+	}
+
+	r.roots[method].insert(path, searchPath, 0)
 	r.handlers[key] = handler
 }
 
-func (r *router) getRoute(method string, path string) {
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchPath := parsePath(path)
+	params := make(map[string]string)
+	node, ok := r.roots[method]
 
+	if !ok {
+		return nil, nil
+	}
+
+	n := node.search(path, searchPath, 0)
+	// The route match
+	if n != nil {
+		// why
+		parts := parsePath(n.pattern)
+		// update params
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchPath[index]
+			}
+			if part[0] == '*' {
+				params[part[1:]] = strings.Join(searchPath[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+	return nil, nil
 }
 
-// func (r *router) list() map[string]HandlerFunc {
-// 	return r.r
-// }
-
 func (r *router) handle(context *Context) {
-	params := parsePath(context.req.URL.Path)
-
 	key := context.req.Method + "-" + context.req.URL.Path
+
 
 	if handler, ok := r.handlers[key]; ok {
 		handler(context)
--- main.go
+++ main.go
@@ -13,7 +13,12 @@ func main() {
 
 	// curl "http://localhost:9999/hello?name=geektutu"
 	eng.GET("/hello", func(context *engine.Context) {
-		context.String("hello %s\n", context.Query("password"))
+		context.String("hello %s\n", context.Query("name"))
+	})
+
+	// expect /hello/geektutu
+	eng.GET("/hello/:name", func(context *engine.Context) {
+		context.String("hello %s ", context.Params("name"))
 	})
 
 	// curl "http://localhost:9999/login" -X POST -d 'username=geektutu&password=1234'
